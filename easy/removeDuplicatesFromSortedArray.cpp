75dbb67de07e96b5bc3c35f1be0f06434a9f525d
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-08-15 20:25:54 +0200

    remove duplicates from sorted array

---
+++ easy/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,17 @@
+int removeDuplicates(vector<int> &nums)
+{
+    int i = 0;
+    int j = 1;
+    while (j < nums.size())
+    {
+        if (nums[i] == nums[j])
+        {
+            j++;
+        }
+        else if (nums[i] != nums[j])
+        {
+            nums[++i] = nums[j];
+        }
+    }
+    return i + 1;
+}
--- easy/squaresOfSortedArray.cpp
+++ easy/squaresOfSortedArray.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 
 vector<int> sortedSquares(vector<int> nums)
+{
+    int length = nums.size();
+    vector<int> result(length);
+    int i = 0;
+    int j = length - 1;
+    int x;
+    int y;
+    while (i <= j)
     {
-        int length = nums.size();
-        vector<int> result(length);
-        int i = 0;
-        int j = length - 1;
-        int x;
-        int y;
-        while (i <= j)
+        x = nums[i] * nums[i];
+        y = nums[j] * nums[j];
+        if (x >= y)
         {
-            x = nums[i] * nums[i];
-            y = nums[j] * nums[j];
-            if (x >= y)
-            {
-                result[length - 1] = x;
-                i++;
-            }
-            else
-            {
-                result[length - 1] = y;
-                j--;
-            }
-            length--;
+            result[length - 1] = x;
+            i++;
         }
-        return result;
+        else
+        {
+            result[length - 1] = y;
+            j--;
+        }
+        length--;
     }
+    return result;
+}
