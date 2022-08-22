b1ce76c75c2cb51c005d427d32ef8b80fd771e60
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-08-22 02:22:47 +0200

    reverse a singly linked list

---
+++ easy/likedList/reverseList.cpp
@@ -0,0 +1,25 @@
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ListNode *reverseList(ListNode *head)
+{
+    struct ListNode *prev = nullptr;
+    struct ListNode *curr = head;
+    struct ListNode *next;
+    while (curr != nullptr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+int main()
+{
+}
