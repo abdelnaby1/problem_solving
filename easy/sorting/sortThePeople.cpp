c121e72f6c893ac0da093138eff2a3857de0f8c1
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-09-28 19:49:06 +0200

    sort the people bubble sort

---
+++ easy/sorting/sortThePeople.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+{
+
+    for (int i = 0; i < heights.size() - 1; i++)
+    {
+        for (int j = 0; j < heights.size() - 1; j++)
+        {
+            if (heights[j] < heights[j + 1])
+            {
+                swap(heights[j], heights[j + 1]);
+                swap(names[j], names[j + 1]);
+            }
+        }
+    }
+    return names;
+}
+int main()
+{
+    return 0;
+}
