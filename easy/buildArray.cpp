e124736de9267593dd1618e88d066c1f12df004a
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-10-05 19:16:47 +0200

    build array from permutation

---
+++ easy/buildArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> buildArray(vector<int> &nums)
+{
+    vector<int> ans(nums.size());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
+}
