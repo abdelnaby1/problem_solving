3477cc1da6b83969840bb2f59ddfa9c99aa86b4e
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-10-08 10:33:00 +0200

    Decode XORed Array

---
+++ easy/decode.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+vector<int> decode(vector<int> &encoded, int first)
+{
+    vector<int> ans(encoded.size() + 1);
+    ans[0] = first;
+    for (int i = 0; i < encoded.size(); i++)
+    {
+        ans[i + 1] = encoded[i] ^ ans[i];
+    }
+    return ans;
+}
