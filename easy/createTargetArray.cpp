70b1b6c3aa3e3d74533fe0fb0de6abe047de8cf6
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2022-10-08 10:42:16 +0200

    Create Target Array in the Given Orde

---
+++ easy/createTargetArray.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+vector<int> createTargetArray(vector<int> &nums, vector<int> &index)
+{
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        ans.insert(ans.begin() + index[i], nums[i]);
+    }
+    return ans;
+}
